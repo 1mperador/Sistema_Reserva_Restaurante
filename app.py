37eaffaa04513365aa38e1a769c5e81e6093c137
Pablo Miguel <105248505+1mperador@users.noreply.github.com>
2025-04-22 21:15:01 -0300

    up

--- app.py
+++ app.py
@@ -1,33 +1,100 @@
 from pymongo import MongoClient 
 from flask import Flask, render_template
 
-app = Flask(__name__)
+#  Sistema do MongoDB
+client      = MongoClient("mongodb://localhost:27017/")
+db          = client.get_database("sistema")
+restaurante = db.get_collection("restaurante")
+usuario     = db.get_collection("usuarios")
+reservas    = db.get_collection("reserva")
 
-@app.route("/base")
-def exibir_home():
-    # Parte principal no projeto 
-    return render_template("base.html")
+def criar_reserva():
+    data = input("Digite a data: ")
+    hora = input("Digite a hora: ")
+    numero_de_pessoas = int(input("Digite quantas pessoas: "))
+
+    nova_reserva = {
+        "data": data,
+        "hora": hora,
+        "numeroDePessoas":numero_de_pessoas
+    }
+
+    reservas.insert_one(nova_reserva)
+    print("Reserva criada")
+
+criar_reserva()
+
+# def limpar():
+#     resultado = reservas.delete_many({})
+#     print(f"🗑️ {resultado.deleted_count} reservas apagadas.")
+# limpar()
+
+
+# filtro = {"status":"livre"}
+
+# vaga_disponivel = reservas.find_one(filtro)
+
+# if vaga_disponivel:
+#     print("✅ Sim, há vagas disponíveis!")
+# else:
+#     print("❌ Não há vagas disponíveis no momento.")
+
+
+# Interface
+# app = Flask(__name__)
+
+# @app.route("/base")
+# def exibir_home():
+#     # Parte principal no projeto 
+#     return render_template("base.html")
   
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
 
-#  Sistema do MongoDB
-con = MongoClient("mongodb://localhost:27017/")
-db = con.get_database("sistema")
-restaurante = db.get_collection("Restaurante")
-usuario = db.get_collection("Usuarios")
-reservas = db.get_collection("Reservas")
 
-# filtro pra identificar o restaurante
-filtro = {"_id": "67e5f70f4024726cc6ef6aac"}
 
-#  Atualização
-atualizacao = {"$set": {"numeroDePessoas": 6}}
+'''
+1º Implemntar logica para vereficar se ha vagas disponivel 
 
-resultado = db.restaurantes.update_one(filtro, atualizacao)
-print(f"Documentos modificados: {resultado.modified_count}")
+2º Adicionar filtro para buscar restaurantes por localização ou tipo de cozinha
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # filtro pra identificar o restaurante
+# filtro = {"_id": "67e5f70f4024726cc6ef6aac"}
+
+# #  Atualização
+# atualizacao = {"$set": {"numeroDePessoas": 6}}
+
+# resultado = db.restaurantes.update_one(filtro, atualizacao)
+# print(f"Documentos modificados: {resultado.modified_count}")
 
 
 # restaurante = {
